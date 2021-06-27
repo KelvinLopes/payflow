fddf2c1a03379c4be606e3d8f19eb35518463ad0
Kelvin Lopes <kelvin7android@gmail.com>
2021-06-27 03:28:39 -0300

    New update and [add animated 🚀 ]

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -22,7 +22,8 @@ class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
     controller.statusNotifier.addListener(
       () {
         if (controller.status.hasBarcode) {
-          Navigator.pushReplacementNamed(context, '/insert_boleto');
+          Navigator.pushReplacementNamed(context, '/insert_boleto',
+              arguments: controller.status.barcode);
         }
       },
     );
